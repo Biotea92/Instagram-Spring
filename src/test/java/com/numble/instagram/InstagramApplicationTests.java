f356cf240995de6425b99e4b1e0ddbd174c05adb
Kim woo jong <skekdnwhd@gmail.com>
2023-03-25 14:58:55 +0900

    init: Instagram project

---
+++ src/test/java/com/numble/instagram/InstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numble.instagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
