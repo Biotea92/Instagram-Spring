111f8c6761203f053a57487d319bfe70cfb413b7
biotea <skekdnwhd@gmail.com>
2023-03-27 00:43:47 +0900

    env : 프로젝트 개발 환경 세팅 (#2)
    * chore: gitignore application-prd.properties
    
    * chore: dependency mysql-connector add
    
    * chore: local properties setting
    
    * chore: test properties add
    
    * chore: ci/cd setting add

---
+++ scripts/start.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+PROJECT_ROOT="/home/ubuntu/app"
+JAR_FILE="$PROJECT_ROOT/spring-instagram.jar"
+
+APP_LOG="$PROJECT_ROOT/application.log"
+ERROR_LOG="$PROJECT_ROOT/error.log"
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+
+TIME_NOW=$(date +%c)
+
+# build 파일 복사
+echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
+cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+
+# jar 파일 실행
+echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
+nohup java -jar -Dspring.profiles.active=prd -Duser.timezone=Asia/Seoul $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
