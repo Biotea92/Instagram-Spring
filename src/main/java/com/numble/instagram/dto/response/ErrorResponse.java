d410df015f7ea11adc7b3bc427c29440669f124b
Kim woo jong <skekdnwhd@gmail.com>
2023-03-27 17:11:41 +0900

    Feat : ControllerAdvice에 CustomException ExceptionHandler에 추가

---
+++ src/main/java/com/numble/instagram/dto/response/ErrorResponse.java
@@ -0,0 +1,15 @@
+package com.numble.instagram.dto.response;
+
+import lombok.Builder;
+
+import java.util.Map;
+
+public record ErrorResponse(int code, String message, Map<String, String> validation) {
+
+    @Builder
+    public ErrorResponse(int code, String message, Map<String, String> validation) {
+        this.code = code;
+        this.message = message;
+        this.validation = validation != null ? validation : Map.of();
+    }
+}
---
+++ src/main/java/com/numble/instagram/presentation/CommonControllerAdvice.java
@@ -0,0 +1,29 @@
+package com.numble.instagram.presentation;
+
+import com.numble.instagram.dto.response.ErrorResponse;
+import com.numble.instagram.exception.CustomException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class CommonControllerAdvice {
+
+    private static final String LOG_FORMAT = "Class={}, Code={}, ErrorMessage={}";
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponse> customException(CustomException e) {
+        int statusCode = e.getStatusCode();
+
+        ErrorResponse body = ErrorResponse.builder()
+                .code(statusCode)
+                .message(e.getMessage())
+                .validation(e.getValidation())
+                .build();
+
+        log.info(LOG_FORMAT, e.getClass().getSimpleName(), e.getStatusCode(), e.getMessage(), e);
+        return ResponseEntity.status(statusCode).body(body);
+    }
+}
