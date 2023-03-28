4cfe9be5420faadfeb47c83eb74a1eccecb230ea
Kim woo jong <skekdnwhd@gmail.com>
2023-03-28 14:40:05 +0900

    refactor : 빌더 클래스 위로 이동

--- src/main/java/com/numble/instagram/application/auth/token/RefreshToken.java
+++ src/main/java/com/numble/instagram/application/auth/token/RefreshToken.java
@@ -1,8 +1,11 @@
 package com.numble.instagram.application.auth.token;
 
+import lombok.Builder;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Builder
 public record RefreshToken(String refreshToken, Long userId, LocalDateTime expiredAt) {
 
     public static RefreshToken create(Long userId, long validTimeInDays) {
