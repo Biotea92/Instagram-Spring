94e8867fabf4edc6904a0280b0c9557f5d5d37b3
Kim woo jong <skekdnwhd@gmail.com>
2023-03-30 01:10:08 +0900

    Feat : 인터셉터와 리졸버 적용

---
+++ src/main/java/com/numble/instagram/config/WebConfig.java
@@ -0,0 +1,52 @@
+package com.numble.instagram.config;
+
+import com.numble.instagram.presentation.auth.AuthenticatedMemberResolver;
+import com.numble.instagram.presentation.auth.AuthenticationInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+import static org.springframework.http.HttpHeaders.LOCATION;
+import static org.springframework.http.HttpHeaders.SET_COOKIE;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthenticatedMemberResolver authenticatedMemberResolver;
+    private final AuthenticationInterceptor authenticationInterceptor;
+    private final static String FRONT_END_LOCAL = "http://localhost:3000";
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authenticatedMemberResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationInterceptor)
+                .addPathPatterns("/api/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedMethods("GET", "POST", "HEAD", "PUT", "PATCH", "DELETE", "TRACE", "OPTIONS")
+                .allowedOrigins(FRONT_END_LOCAL)
+                .allowCredentials(true)
+                .exposedHeaders(LOCATION, SET_COOKIE);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
