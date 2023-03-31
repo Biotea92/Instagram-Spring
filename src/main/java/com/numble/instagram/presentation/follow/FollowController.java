b01f20d6889840c99dfdab57074b5c7820ff1cf9
Kim woo jong <skekdnwhd@gmail.com>
2023-04-01 01:26:55 +0900

    Feat : 언팔로우 기능 추가

--- src/main/java/com/numble/instagram/presentation/follow/FollowController.java
+++ src/main/java/com/numble/instagram/presentation/follow/FollowController.java
@@ -1,6 +1,7 @@
 package com.numble.instagram.presentation.follow;
 
 import com.numble.instagram.application.usecase.CreateFollowUserUsecase;
+import com.numble.instagram.application.usecase.DestroyFollowUserUsecase;
 import com.numble.instagram.dto.common.FollowDto;
 import com.numble.instagram.presentation.auth.AuthenticatedUser;
 import com.numble.instagram.presentation.auth.Login;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FollowController {
 
     private final CreateFollowUserUsecase createFollowUserUsecase;
+    private final DestroyFollowUserUsecase destroyFollowUserUsecase;
 
     @Login
     @PostMapping("/{toUserId}")
@@ -23,5 +25,13 @@ public class FollowController {
                             @PathVariable Long toUserId) {
         Long savedToUserId = createFollowUserUsecase.execute(fromUserId, toUserId);
         return FollowDto.from(savedToUserId);
+    }
+
+    @Login
+    @PostMapping("/unfollow/{toUserId}")
+    public FollowDto unfollow(@AuthenticatedUser Long fromUserId,
+                            @PathVariable Long toUserId) {
+        Long deletedToUserId = destroyFollowUserUsecase.execute(fromUserId, toUserId);
+        return FollowDto.from(deletedToUserId);
     }
 }
