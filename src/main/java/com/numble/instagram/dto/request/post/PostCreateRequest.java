de3710eb74fb6dbe181e92e98c74a50e590b8a36
Kim woo jong <skekdnwhd@gmail.com>
2023-04-03 16:49:42 +0900

    Fix : multipart validation을 어노테이션으로 변경

--- src/main/java/com/numble/instagram/dto/request/post/PostCreateRequest.java
+++ src/main/java/com/numble/instagram/dto/request/post/PostCreateRequest.java
@@ -1,20 +1,8 @@
 package com.numble.instagram.dto.request.post;
 
-import com.numble.instagram.exception.badrequest.ImageFileNotExistsException;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.web.multipart.MultipartFile;
 
-public record PostCreateRequest(@NotBlank String content, MultipartFile postImageFile) {
-
-    public PostCreateRequest(String content, MultipartFile postImageFile) {
-        this.content = content;
-        validatePostImageFile(postImageFile);
-        this.postImageFile = postImageFile;
-    }
-
-    private void validatePostImageFile(MultipartFile postImageFile) {
-        if (postImageFile == null || postImageFile.isEmpty()) {
-            throw new ImageFileNotExistsException();
-        }
-    }
+public record PostCreateRequest(@NotBlank String content, @NotNull MultipartFile postImageFile) {
 }
