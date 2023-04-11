1eff5fd59d3de5b24f4c411aa0176d6be1765601
Kim woo jong <skekdnwhd@gmail.com>
2023-04-12 05:37:41 +0900

    Feat : DM 기능 repository 추가

---
+++ src/main/java/com/numble/instagram/domain/dm/repository/MessageRepository.java
@@ -0,0 +1,7 @@
+package com.numble.instagram.domain.dm.repository;
+
+import com.numble.instagram.domain.dm.entity.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+}
